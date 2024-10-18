ff9821614f998fbae7e36b22701ff0436c14a778
Latif Altay <latifaltay00@gmail.com>
2024-10-18 23:56:45 +0300

    The Product and Category entities have been completed

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductDetail
+    {
+
+        public string Id { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductInfo { get; set; }
+    }
+}
