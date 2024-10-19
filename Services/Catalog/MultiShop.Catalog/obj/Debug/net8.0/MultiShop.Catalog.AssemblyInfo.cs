eb41c3706a18cadfafd472a4ecd0a7c93b284bfb
Latif Altay <latifaltay00@gmail.com>
2024-10-19 19:11:29 +0300

    database settings files have been created

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff9821614f998fbae7e36b22701ff0436c14a778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51990c1a26a50f9f81df2de65fa97207e8799688")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
