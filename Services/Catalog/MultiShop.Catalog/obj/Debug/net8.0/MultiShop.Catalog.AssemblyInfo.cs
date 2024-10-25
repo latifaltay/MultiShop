d5b873b716bb2263ac1b14264ee62dd28605deeb
Latif Altay <latifaltay00@gmail.com>
2024-10-25 06:45:33 +0300

    identity microservice has been created, configured and some features added and developed

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a05df8bac04ca6309205787eb376a559cdb46dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db94bbdd54f7fc4f3ad97780137d9c4ca411bcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
