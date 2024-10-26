fe65e4ae2b56aa442296bf6c1eb6c2e95e96b5c4
Latif Altay <latifaltay00@gmail.com>
2024-10-27 00:19:26 +0300

    Basket microservice dtos have been created

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7557a71f1654647fefe32ddb2ed64319ce5d0b75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8bec52911d4dd000c0fc2e2477b615c641e1781")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
