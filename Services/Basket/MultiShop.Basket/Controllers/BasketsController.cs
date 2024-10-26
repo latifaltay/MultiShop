baf4c84cb21f16ef287a6df6cfe3ca7ad78ef9fa
Latif Altay <latifaltay00@gmail.com>
2024-10-27 01:17:46 +0300

    Basket controller has been completed

--- Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -1,11 +1,39 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Basket.Dtos;
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
 
 namespace MultiShop.Basket.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BasketsController : ControllerBase
+    public class BasketsController(IBasketService _basketService, ILoginService _loginService) : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasketDetail() 
+        {
+            var values = await _basketService.GetBasket(_loginService.GetUserId);
+            return Ok(values);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto) 
+        {
+            basketTotalDto.UserId = _loginService.GetUserId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Sepetteki değişiklikler kaydedildi");
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket() 
+        {
+            await _basketService.DeleteBasket(_loginService.GetUserId);
+            return Ok("Sepet başarıyla silindi");
+        }
+
+
     }
 }
