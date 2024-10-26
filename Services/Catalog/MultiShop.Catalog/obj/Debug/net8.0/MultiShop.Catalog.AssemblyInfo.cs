7869e13056cab04abe9197ebda6620bb19c90403
Latif Altay <latifaltay00@gmail.com>
2024-10-26 05:20:40 +0300

    repository design pattern has been applicated

--- Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
+++ Services/Catalog/MultiShop.Catalog/obj/Debug/net8.0/MultiShop.Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0fbcc6bb5d3f38cd6a11730b50ce9851f6c1c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7557a71f1654647fefe32ddb2ed64319ce5d0b75")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop.Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
