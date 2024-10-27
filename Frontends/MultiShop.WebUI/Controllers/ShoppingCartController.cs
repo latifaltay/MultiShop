3541ccfe319b78f4f0135f0324f33c7b19063374
Latif Altay <latifaltay00@gmail.com>
2024-10-27 23:18:09 +0300

    Everything up to the admin panel is complete

--- Frontends/MultiShop.WebUI/Controllers/ShoppingCartController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ShoppingCartController.cs
@@ -2,17 +2,11 @@
 
 namespace MultiShop.WebUI.Controllers
 {
-    public class ProductListController : Controller
+    public class ShoppingCartController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult ProductDetail() 
-        {
-            return View();
-        }
-
     }
 }
